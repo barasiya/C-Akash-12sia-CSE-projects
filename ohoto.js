cdd58e6e870af22ab8000428b3898a674f62eee4
aakash barasiya <barasia2001@gmail.com>
2025-02-22 12:23:42 +0530

    Update ohoto.js

--- ohoto.js
+++ ohoto.js
@@ -4,3 +4,27 @@ function toggleMenu() {
     const nav = document.querySelector('.nav-links');
     nav.style.display = nav.style.display === 'flex' ? 'none' : 'flex';
 }
+
+
+function openLightbox(src) {
+    const lightbox = document.getElementById('lightbox');
+    const img = document.getElementById('lightbox-img');
+    const video = document.getElementById('lightbox-video');
+
+    img.src = src;
+    img.style.display = 'block';
+    video.style.display = 'none';
+    lightbox.classList.add('active');
+}
+
+function openLightboxVideo(videoElement) {
+    const lightbox = document.getElementById('lightbox');
+    const img = document.getElementById('lightbox-img');
+    const video = document.getElementById('lightbox-video');
+
+    video.src = videoElement.src;
+    video.style.display = 'block';
+    video.play();
+    img.style.display = 'none';
+    lightbox.classList.add('active');
+}
